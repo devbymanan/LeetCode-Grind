d5c97c12180612535ce097c751d64c8ffe429ebe
DevByManan <devbymanan@gmail.com>
2026-08-11 02:28:18 -0700

    Time: 730 ms (26.55%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+SELECT sp.name
+FROM SalesPerson AS sp
+WHERE sp.sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders AS o
+    JOIN Company AS c
+        ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
